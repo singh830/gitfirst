d5b86219d248cc55d2676ff1993d6ad399db73b5
singh830 <singhsuhani830>
2023-12-21 14:30:40 +0530

    second

---
+++ getelementbyid.js
@@ -0,0 +1,13 @@
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '2px solid orange';
+
+const mainheading = document.getElementById('main-heading');
+mainheading.textContent = 'Fruit World';
+mainheading.style.color = 'orange';
+
+const basketheading = document.getElementById('basket-heading');
+basketheading.style.color = 'green';
+
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>'
